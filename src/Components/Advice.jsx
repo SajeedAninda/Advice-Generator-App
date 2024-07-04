227dbf683f5fcb987569a32289c097fcbe3625dc
SajeedAninda <sajaninda@gmail.com>
2024-07-04 11:58:18 +0600

    Fetched data from api

--- src/Components/Advice.jsx
+++ src/Components/Advice.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import patternDivider from "../assets/pattern-divider-desktop.svg"
 import dice from "../assets/icon-dice.svg"
 
 const Advice = () => {
+    let [advice, setAdvice] = useState({});
+
+    useEffect(() => {
+        fetch('https://api.adviceslip.com/advice')
+            .then(response => response.json())
+            .then(json => setAdvice(json.slip));
+
+    }, [])
+
+
     return (
         <div className='h-screen w-full bg-[#1f2632] flex justify-center items-center'>
             <div className='bg-[#323a49] h-fit px-12 pt-12 pb-20 w-[40%] rounded-2xl flex flex-col items-center text-center relative'>
                 <p className='text-[#52ffa8] text-[14px] font-semibold tracking-[5px]'>
-                    ADVICE ID # 177
+                    ADVICE ID # {advice.id}
                 </p>
 
                 <h3 className='mt-4 text-[28px] font-bold text-[#CEE3E9]'>
-                    “Everyone has their down days. Don't take it out on innocent bystanders.”
+                    “{advice.advice}”
                 </h3>
 
                 <img className='mt-8' src={patternDivider} alt="" />
